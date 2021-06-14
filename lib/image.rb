7dce9ac33c5303d3569fddaf7b543533f8b382bc
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-06-14 20:15:00 +0200

    change image archive domain

--- lib/image.rb
+++ lib/image.rb
@@ -4,7 +4,7 @@ require_relative 'network'
 class Image
   def self.get(filter_param, page = 1)
     query = Filter.build(filter_param)
-    url = "http://www.bildarchiv-js.com/api/images.json?sort=-Jahr&per_page=30&page=#{page}&#{query}"
+    url = "http://www.bildarchiv-js.ch/api/images.json?sort=-Jahr&per_page=30&page=#{page}&#{query}"
     Network.get_json(url)
   end
 end
