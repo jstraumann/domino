0ea8bec0b340e4e71232c703f4b681f1163cae36
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-06-21 14:36:09 +0200

    add failsafe

--- lib/image.rb
+++ lib/image.rb
@@ -9,11 +9,11 @@ class Image
     codes = Filter.codes
     data.each do |d|
       t_match = d['Techniken'].split(' ').map{ |t| puts t.inspect; codes.find{ |c| c.Column == 'Technik' && c.Code == t } }
-      d['Techniken'] = t_match.empty? ? '-' : t_match.map{ |x| x.Title }.join(', ')
+      d['Techniken'] = t_match.empty? ? '-' : t_match.map{ |x| x.nil? ? '-' : x.Title }.join(', ')
       m_match = d['Motiven'].split(' ').map{ |t| codes.find{ |c| c.Column == 'Motiven' && c.Code == t } }
-      d['Motiven'] = m_match.empty? ? '-' : m_match.map{ |x| x.Title }.join(', ')
+      d['Motiven'] = m_match.empty? ? '-' : m_match.map{ |x| x.nil? ? '-' : x.Title }.join(', ')
       d_match = d['Darstellungsformen'].split(' ').map{ |t| codes.find{ |c| c.Column == 'Darstellungsformen' && c.Code == t } }
-      d['Darstellungsformen'] = d_match.empty? ? '-' : d_match.map{ |x| x.Title }.join(', ')
+      d['Darstellungsformen'] = d_match.empty? ? '-' : d_match.map{ |x| x.nil? ? '-' : x.Title }.join(', ')
     end
     return data
   end
