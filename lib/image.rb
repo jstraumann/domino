5d92d14686dd1c1da72a4603fcea9aa5fec1c57d
Lukas_Skywalker <lukas.diener@hotmail.com>
2021-06-21 15:05:59 +0200

    larger images

--- lib/image.rb
+++ lib/image.rb
@@ -8,7 +8,7 @@ class Image
     data = Network.get_json(url)
     codes = Filter.codes
     data.each do |d|
-      t_match = d['Techniken'].split(' ').map{ |t| puts t.inspect; codes.find{ |c| c.Column == 'Technik' && c.Code == t } }
+      t_match = d['Techniken'].split(' ').map{ |t| codes.find{ |c| c.Column == 'Technik' && c.Code == t } }
       d['Techniken'] = t_match.empty? ? '-' : t_match.map{ |x| x.nil? ? '-' : x.Title }.join(', ')
       m_match = d['Motiven'].split(' ').map{ |t| codes.find{ |c| c.Column == 'Motiven' && c.Code == t } }
       d['Motiven'] = m_match.empty? ? '-' : m_match.map{ |x| x.nil? ? '-' : x.Title }.join(', ')
